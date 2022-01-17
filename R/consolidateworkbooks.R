3acc30ebb464b6da37397a8f5464254cbc9e9848
markconnolly <w.mark.connolly@gmail.com>
2022-01-17 08:17:57 -0500

    user feedback at end, better documentation

--- R/consolidateworkbooks.R
+++ R/consolidateworkbooks.R
@@ -3,9 +3,10 @@ consolidateworkbooks <- function(verbose = FALSE)
   #' Consolidate multiple Excel workbooks into a single workbook file
   #'
   #' consolidateworkbooks is useful for combining multiple
-  #' Excel workbooks and their contained sheet into a single Excel file.
+  #' Excel workbooks and their contained sheets into a single Excel file.
   #' This function was coded with the assumption all of the workbooks
-  #' and their contained sheets have the same structure.
+  #' and their contained sheets have the same structure.  The user is
+  #' prompted for source files and target file name.
   #'
   #' @param verbose when TRUE display processing information
   #'
@@ -26,12 +27,10 @@ consolidateworkbooks <- function(verbose = FALSE)
   if (length(list_of_files) == 0)
     stopquietly("no source files selected")
 
-  targetfilename <- choose.files(
-    multi = FALSE,
-    filters = excelfilter,
-    default = paste(dirname(list_of_files[1]), "/consolidate.xlsx", sep = ""),
-    caption = "Set target for consolidated files"
-  )
+  targetfilename <- choose.files(filters = excelfilter,
+                                 multi = FALSE,
+                                 default = paste(dirname(list_of_files[1]), "/consolidate.xlsx", sep = ""),
+                                 caption = "Set target for consolidated files")
 
   if (length(targetfilename) == 0)
     stopquietly("no target file named")
@@ -42,4 +41,6 @@ consolidateworkbooks <- function(verbose = FALSE)
          verbose = verbose) %>%
     do.call('rbind', .) %>%
     write_xlsx(path = targetfilename)
+
+  message(paste("\nwrote "), targetfilename)
 }
