619daefe6da4b4a3191f0b8f6d6e673e92b64a02
markconnolly <w.mark.connolly@gmail.com>
2022-01-21 06:09:53 -0500

    improved documentation

--- R/consolidatesheets.R
+++ R/consolidatesheets.R
@@ -1,9 +1,13 @@
 consolidatesheets <- function(path, verbose = FALSE) {
   #' Consolidate sheets of an Excel workbook into a single data frame
   #'
-  #' `consolidatesheets` is useful for combining multiple sheets of an
+  #' `consolidatesheets` combines multiple sheets of an
   #' Excel workbook into a single data frame.  It treats all columns as text
-  #' and assumes that the sheets all have the same structure.
+  #' and assumes that the sheets all have the same structure.  The sheets
+  #' are read one at a time, converted to a data frame for each sheet, and
+  #' collected in a list.  The data frames in the list are bound together
+  #' into a single data frame, which is returned to the caller.  If `verbose`
+  #' is `TRUE`, the intermediate list of data frames is displayed.
   #'
   #' @param path file name of the source Excel workbook
   #' @param verbose when `TRUE` display `str` of returned data frame
@@ -15,10 +19,7 @@ consolidatesheets <- function(path, verbose = FALSE) {
   #'
   #' @export
 
-  lapply(excel_sheets(path),                      # collection of sheet names
-         read_excel,                              # the read function for Excel files
-         col_types = "text", path = path) %T>%    # extra arguments for the read function
-    {if (verbose) str(.)}  %>%                    # for manual compare with final results
-    do.call('rbind', .)                           # bind the data frames in the list into
-                                                  # a single data frame and return it to the caller
+  lapply(excel_sheets(path), read_excel, col_types = "text", path = path) %T>%
+    {if (verbose) str(.)}  %>%
+    do.call('rbind', .)
 }
